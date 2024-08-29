223587c68e38d28447497d03282f1ced746d907a
divine <divinemaina@gmail.com>
2024-08-29 18:38:18 +0200

    add environment variables

--- news-app/src/app/new-service.service.ts
+++ news-app/src/app/new-service.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from '../environments/environment.development';
 @Injectable({
   providedIn: 'root'
 })
 export class NewServiceService {
 
-private apiUrl = 'https://newsapi.org/v2/everything?q=tesla&from=2024-07-29&sortBy=publishedAt&apiKey=4a879e44437b4c34986684492e1314e0';
 
 constructor(private http: HttpClient) { }
 
 getNews(): Observable<any> {
-  return this.http.get<any>(this.apiUrl);
+  return this.http.get<any>(environment.apiUrl);
 }
 getNewsById(id: string): Observable<any> {
-  return this.http.get<any>(`https://newsapi.org/v2/everything?q=tesla&from=2024-07-29&sortBy=publishedAt&apiKey=4a879e44437b4c34986684492e1314e0/${id}`);
+  return this.http.get<any>(`environment.apiUrl/${id}`);
 }
 
 }
